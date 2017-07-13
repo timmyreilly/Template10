b0d66fc4060e99179c66c57a6b61a2cc09b9d1ec
Tim Reilly <timreilly@live.com>
2017-07-13 16:11:22 -0700

    back to backservice

---
+++ Helpers/Template10.CodeBehind/Common.PropertyBag/IPropertyBagAsync.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Template10.Portable.Common
+{
+    public interface IPropertyBagAsync
+    {
+        event PropertyChangedEventHandler MapChanged;
+
+        Task<string[]> AllKeysAsync();
+        Task<bool> ContainsKeyAsync(string key);
+
+        Task<T> GetValueAsync<T>(string key);
+        Task<string> GetValueAsync(string key);
+
+        Task<(bool Success, string Value)> TryGetValueAsync(string key);
+        Task<(bool Success, T Value)> TryGetValueAsync<T>(string key);
+
+        Task SetValueAsync<T>(string key, T value);
+        Task SetValueAsync(string key, string value);
+
+        Task ClearAsync();
+    }
+}
